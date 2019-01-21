995ab6c7f4b9589bf420eca0de285a6aedf63d28
manisha1896 <manisha.4918@gmail.com>
2019-01-21 17:17:17 +0530

    modified message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "hello world" });
+  res.status(200).json({ message: "Learning git" });
 });
 
 app.listen(3000, () => {

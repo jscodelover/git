8e451471ebcbbcc3e159a66c794d59806ec53f3c
manisha1896 <manisha.4918@gmail.com>
2019-01-21 17:16:06 +0530

    added gitignore & initial setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "hello world" });
+});
+
+app.listen(3000, () => {
+  console.log("running at port", 3000);
+});

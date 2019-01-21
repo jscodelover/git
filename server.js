358523c743fc3d9c6a765a94ee574e9a18340cca
manisha1896 <manisha.4918@gmail.com>
2019-01-21 17:23:42 +0530

    added welcome message

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Learning git" });
 });
 
+app.get("/welcome", (req, res) => {
+  res.status(200).json({ message: "welcome to git" });
+});
+
 app.listen(3000, () => {
   console.log("running at port", 3000);
 });

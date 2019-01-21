56028ecab6b7ef0057e7ca4bdee93ce0972ae02b
manisha1896 <manisha.4918@gmail.com>
2019-01-21 17:25:59 +0530

    minor changes

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Learning git" });
+  res.status(200).json({ message: "Start learning git" });
 });
 
 app.listen(3000, () => {
-  console.log("running at port", 3000);
+  console.log("server running at port", 3000);
 });
